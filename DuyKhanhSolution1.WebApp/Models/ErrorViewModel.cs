187bf09ab18c237bbfe00b9b57b0ec52e198feb0
pham duy khanh <duykhanhuctdt4@gmail.com>
2020-03-25 00:17:54 +0700

    Add project files.

---
+++ DuyKhanhSolution1.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DuyKhanhSolution1.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
