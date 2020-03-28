2f385ca844968e0676298163a1a83926d204b9da
pham duy khanh <duykhanhuctdt4@gmail.com>
2020-03-28 16:42:59 +0700

    update all Configurations

--- DuyKhanhSolution1.Data/EF/DuyKhanhShopDBContext.cs
+++ DuyKhanhSolution1.Data/EF/DuyKhanhShopDBContext.cs
@@ -17,7 +17,19 @@ namespace DuyKhanhSolution1.Data.EF
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
             modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
