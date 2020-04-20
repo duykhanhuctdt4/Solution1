0321cf7d9cba3589057663f9f974c0fa733390e8
pham duy khanh <duykhanhuctdt4@gmail.com>
2020-04-20 22:24:50 +0700

    GetAllPaging

---
+++ DuyKhanhShop.Ultilities/Exceptions/DuyKhanhCustomExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuyKhanhShop.Ultilities.Exceptions
+{
+    public class DuyKhanhCustomExceptions : Exception
+    {
+        public DuyKhanhCustomExceptions()
+        {
+        }
+
+        public DuyKhanhCustomExceptions(string message)
+            : base(message)
+        {
+        }
+
+        public DuyKhanhCustomExceptions(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
--- DuyKhanhSolution1.Application/Catalog/Products/ManageProductService.cs
+++ DuyKhanhSolution1.Application/Catalog/Products/ManageProductService.cs
@@ -1,12 +1,15 @@
-﻿using DuyKhanhSolution1.Application.Catalog.Products.Dtos;
+﻿using DuyKhanhShop.Ultilities.Exceptions;
+using DuyKhanhSolution1.Application.Catalog.Products.Dtos;
 using DuyKhanhSolution1.Application.Catalog.Products.Dtos.Manage;
 using DuyKhanhSolution1.Application.Dtos;
 using DuyKhanhSolution1.Data.EF;
 using DuyKhanhSolution1.Data.Entities;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DuyKhanhSolution1.Application.Catalog.Products
 {
@@ -58,9 +61,9 @@ namespace DuyKhanhSolution1.Application.Catalog.Products
         {
             var product = await _context.Products.FindAsync(productId);
             if(product == null)
-                thow new Ex
+                throw new DuyKhanhCustomExceptions($"Can not find a product: {productId}");
             _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
+            return await _context.SaveChangesAsync();
         }
 
         public async Task<List<ProductViewModel>> GetAll()
@@ -68,9 +71,51 @@ namespace DuyKhanhSolution1.Application.Catalog.Products
             throw new NotImplementedException();
         }
 
-        public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
+        public  async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
         {
-            throw new NotImplementedException();
+            // 1. Select join
+            var query = from p   in _context.Products
+                        join pt  in _context.ProductTranslations    on p.Id              equals pt.ProductId
+                        join pic in _context.ProductInCategories    on p.Id              equals pic.ProductId
+                        join c   in _context.Categories             on pic.CategoryId    equals c.Id
+                        select new { p, pt , pic };
+            // 2.filter
+            if (!string.IsNullOrEmpty(request.Keyword))
+            {
+                query = query.Where(x => x.pt.Name.Contains(request.Keyword));
+            }
+            if(request.CategoryIds.Count >0)
+            {
+                query = query.Where(p => request.CategoryIds.Contains(p.pic.CategoryId));
+            }
+            //3. Paging
+            int totalRow = await query.CountAsync();
+            var data = await query.Skip((request.PageIndex - 1)* request.PageSize).
+                Take(request.PageSize)
+                .Select(x=>new ProductViewModel()
+                { 
+                    Id = x.p.Id,
+                    Name = x.pt.Name,
+                    DateCreate = x.p.DateCreate,
+                    Description = x.pt.Description,
+                    Details = x.pt.Details,
+                    LanguageId = x.pt.LanguageId,
+                    OriginalPrice = x.p.OriginalPrice,
+                    Price = x.p.Price,
+                    SeoAlias = x.pt.SeoAlias,
+                    SeoDescription = x.pt.SeoDescription,
+                    SeoTitle = x.pt.SeoTitle,
+                    Stock = x.p.Stock,
+                    ViewCount = x.p.ViewCount
+                }).ToListAsync();
+
+            //4. Select and project
+            var pageResult = new PagedResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+            return pageResult;
         }
 
         public async Task<int> Update(ProducUpdateRequest request)
